b60c2991e5295195041ee59d316d15bdbf56ab0d
Gogo <gogo81745.moe@gmail.com>
2019-12-08 22:16:10 +0800

    fix left button cannot read

--- src/key_scanner.v
+++ src/key_scanner.v
@@ -14,7 +14,7 @@ reg [3:0] state;
 reg[15:0] data;
 reg[15:0] data_out;
 
-reg waitbit;
+reg[2:0] waitbit; // make butten wait some clock to read data
 
 key_mapping key_mapping_inst(data_out, keys);
 
@@ -35,49 +35,49 @@ always @(posedge clk) begin
             SCANING: begin
                 case(col)
                     4'b0111: begin
-                        if(waitbit == 1) begin
+                        if(waitbit == 7) begin
                             data[15:12] <= row;
                             col <= 4'b1011;
                             waitbit <= 0;
                         end
-                        else waitbit <= 1;
+                        else waitbit <= waitbit + 1;
                     end
                     4'b1011: begin
-                        if(waitbit == 1) begin
+                        if(waitbit == 7) begin
                             data[11:8] <= row;
                             col <= 4'b1101;
                             waitbit <= 0;
                         end
-                        else waitbit <= 1;
+                        else waitbit <= waitbit + 1;
                     end
                     4'b1101: begin
-                        if(waitbit == 1) begin
+                        if(waitbit == 7) begin
                             data[7:4] <= row;
                             col <= 4'b1110;
                             waitbit <= 0;
                         end
-                        else waitbit <= 1;
+                        else waitbit <= waitbit + 1;
                     end
                     4'b1110: begin
-                        if(waitbit == 1) begin
+                        if(waitbit == 7) begin
                             data[3:0] <= row;
                             col <= 4'b1111;
                             waitbit <= 0;
                             state <= UPDATE_DATA;
                         end
-                        else waitbit <= 1;
+                        else waitbit <= waitbit + 1;
 
                     end
                 endcase
 
             end
             UPDATE_DATA: begin
-                if(waitbit == 1) begin
+                if(waitbit == 7) begin
                     state <= NO_SCAN;
                     data_out <= data;
                     waitbit <= 0;
                 end
-                else waitbit <= 1;
+                else waitbit <= waitbit + 1;
             end
 
         endcase
